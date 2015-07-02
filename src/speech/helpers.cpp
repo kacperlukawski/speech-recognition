9cb2f16d73b1b5cd5fab6d8ee9e92b88f571d4e0
kacper <kacper.lukawski@uj.edu.pl>
2015-07-02 20:10:46 +0200

    Refactoring of helpers

--- src/speech/helpers.cpp
+++ src/speech/helpers.cpp
@@ -31,3 +31,46 @@ std::valarray<double> minItems(std::valarray<double> &v1, std::valarray<double> 
 
     return std::move(result);
 }
+
+std::vector<char> getUniqueLetters(std::vector<std::string> &transcriptions) {
+    std::vector<char> letters;
+    for (auto it = transcriptions.begin(); it != transcriptions.end(); it++) {
+        int length = (*it).size();
+        for (int i = 0; i < length; i++) {
+            char letter = (*it)[i];
+
+            // blank characters are not included
+            if (letter == ' ' || letter == '\t' || letter == '\n' || letter == '\r') {
+                continue;
+            }
+
+            auto pos = std::find(letters.begin(), letters.end(), letter);
+            if (pos != letters.end()) {
+                continue;
+            }
+
+            letters.push_back(letter);
+        }
+    }
+
+    return letters;
+}
+
+std::string getFileContent(const std::string &filepath) {
+    std::ifstream file(filepath.c_str());
+    std::string content = std::string(std::istreambuf_iterator<char>(file),
+                                      std::istreambuf_iterator<char>());
+    file.close();
+
+    return content;
+}
+
+void str_replace(std::string &s, const std::string &search, const std::string &replace) {
+    for (size_t pos = 0; ; pos += replace.length()) {
+        pos = s.find(search, pos);
+        if (pos == string::npos) break;
+
+        s.erase(pos, search.length());
+        s.insert(pos, replace);
+    }
+}
--- src/speech/helpers.h
+++ src/speech/helpers.h
@@ -23,4 +23,27 @@ std::valarray<double> maxItems(std::valarray<double> &v1, std::valarray<double> 
  */
 std::valarray<double> minItems(std::valarray<double> &v1, std::valarray<double> &v2);
 
+/**
+ * Gets the vector containing all letters occurring in given collection
+ * of words. The result does not contain duplicates an blank characters.
+ * @param transcriptions vector of texts
+ * @return vector containing all characters occurred in given texts
+ */
+std::vector<char> getUniqueLetters(std::vector<std::string> &transcriptions);
+
+/**
+ * Loads whole content of the given file and returns it as a string
+ * @param filepath path of the file to be read
+ * @return file content
+ */
+std::string getFileContent(const std::string &filepath);
+
+/**
+ * Replaces all occurrences of the given pattern
+ * @param s string where we want to replace pattern in
+ * @param search pattern to be replaces
+ * @param replace replacement string
+ */
+void str_replace(std::string &s, const std::string &search, const std::string &replace);
+
 #endif //SPEECH_RECOGNITION_HELPERS_H

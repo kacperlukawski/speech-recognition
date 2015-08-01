6384383121a08634817e3845e36b8058b62334d0
Szymon <szymonwenta@gmail.com>
2015-08-01 21:28:05 +0200

    Merge.

--- src/main.cpp
+++ src/main.cpp
@@ -47,215 +47,48 @@ using speech::vectorizer::FormantVectorizer;
 using speech::vectorizer::ThirdsPowerVectorizer;
 using speech::vectorizer::MFCCVectorizer;
 
-//
-// Gets the vector containing all letters occuring in given collection
-// of words. The result does not contain duplicates an blank characters.
-//
-std::vector<char> getUniqueLetters(std::vector<std::string> &transcriptions) {
-    std::vector<char> letters;
-    for (auto it = transcriptions.begin(); it != transcriptions.end(); it++) {
-        int length = (*it).size();
-        for (int i = 0; i < length; i++) {
-            char letter = (*it)[i];
+#include "speech/HMMLexicon.h"
 
-            // blank characters are not included
-            if (letter == ' ' || letter == '\t' || letter == '\n' || letter == '\r') {
-                continue;
-            }
+using speech::HMMLexicon;
 
-            auto pos = std::find(letters.begin(), letters.end(), letter);
-            if (pos != letters.end()) {
-                continue;
-            }
+#include "speech/helpers.h"
 
-//            std::cout << letter << " ";
-
-            letters.push_back(letter);
-        }
-    }
-
-    return letters;
-}
-
-void str_replace(string &s, const string &search, const string &replace) {
-    for (size_t pos = 0; ; pos += replace.length()) {
-        pos = s.find(search, pos);
-        if (pos == string::npos) break;
-
-        s.erase(pos, search.length());
-        s.insert(pos, replace);
-    }
-}
-
-/**
- * Loads whole content of the given file and returns it as a string
- *
- * @return file content
- */
-std::string getFileContent(const std::string &filepath) {
-    std::ifstream file(filepath.c_str());
-    std::string content = std::string(std::istreambuf_iterator<char>(file),
-                                      std::istreambuf_iterator<char>());
-    file.close();
-
-    return content;
-}
-
-void showValues(DataSample<short> sample) {
-    for (int i = 0; i < sample.getSize(); ++i) {
-        std::cout << sample.getValues().get()[i] << " ";
-    }
-    std::cout << std::endl;
-    std::cout << std::endl;
-}
-
-/**
- * This is an entry point of the application. It allows user to select the data source used
- * to get an input data, choose the methods of signal transformation into frequency domain
- * and a spelling transcription.
- *
- * @todo create a logic
- */
 int main(int argc, char **argv) {
+    // Get the list of the words' transcriptions and assigned files
+    // Load all the files and
 
-    auto wavHeader = WaveFileDataSource<short>::createPcmWavHeader(1, 44100, 16);
 
-    WaveFileDataSource<short> newDt = WaveFileDataSource<short>(wavHeader, 20);
+    const int SAMPLE_LENGTH = 20; // in milliseconds
+    const int MFCC_BINS = 26; // number of Mel filters in a bank
+    const int MFCC_CEPSTRAL_COEFFICIENTS = 12; // number of cepstral coefficients used for analysis
+    const double MFCC_MIN_FREQUENCY = 64.0; // in Herzs
+    const double MFCC_MAX_FREQUENCY = 10000.0; // in Herzs
+    const int LEXICON_DIMENSIONALITY = 3 * (MFCC_CEPSTRAL_COEFFICIENTS + 1);
+    const int LEXICON_GAUSSIANS = 16;
 
-    WaveFileDataSource<short> dt = WaveFileDataSource<short>(
-            "/home/szymon/Pobrane/sample/test_mono_44100Hz_16bit_PCM.wav", 20);
+    HMMLexicon lexicon(LEXICON_DIMENSIONALITY, LEXICON_GAUSSIANS);
+    MFCCVectorizer<short int> *mfccVectorizer = new MFCCVectorizer<short int>(MFCC_BINS, MFCC_CEPSTRAL_COEFFICIENTS,
+                                                                              MFCC_MIN_FREQUENCY, MFCC_MAX_FREQUENCY);
 
-//    auto begin = dt.getSamplesIteratorBegin();
-//    auto end = dt.getSamplesIteratorEnd();
-
-//    FastFourierTransform<short> fft = FastFourierTransform<short>();
-//
-//    for(; begin != end; begin++) {
-//        auto fftSample = fft.transform(*begin);
-//        newDt.addSample(fft.reverseTransform(fftSample));
-//    }
-
-    auto begin = dt.getOffsetIteratorBegin(20, 0);
-    auto end = dt.getOffsetIteratorEnd();
-
-    for (; begin != end; ++begin) {
-        //showValues(*begin);
-
-        newDt.addSample(*begin);
+    std::string filename("/home/kacper/Test/sala_21.wav");
+    std::string transcription("s|a|l|a");
+    // do the following process for each file from the collection
+    {
+        WaveFileDataSource<short int> dataSource(filename, SAMPLE_LENGTH);
+        HMMLexicon::Observation utterance = mfccVectorizer->vectorize(dataSource);
+        lexicon.addUtterance(utterance, transcription, "|");
     }
 
-//    for (int i = 0; i < 500; ++i, ++begin) {
-//        newDt.addSample(*begin);
-//    }
+    lexicon.fit();
 
-    newDt.saveToFile("/home/szymon/Pobrane/sample/offsetTest.wav");
-
-
-
-    /*const int singleSampleLength = 20;           // length of the DataSample in milliseconds
-    const int numberOfPhonems = 91;              // number of clusters used by the clustering method
-
-    // @todo: list should be more dynamic, but it's not necessary now
-    std::vector<const char *> sourceFiles;
-    sourceFiles.push_back("/home/szymon/ClionProjects/speech-recognition/dataset/records/record_0.wav");
-    sourceFiles.push_back("/home/szymon/ClionProjects/speech-recognition/dataset/records/record_1.wav");
-    sourceFiles.push_back("/home/szymon/ClionProjects/speech-recognition/dataset/records/record_2.wav");
-    sourceFiles.push_back("/home/szymon/ClionProjects/speech-recognition/dataset/records/record_3.wav");
-    sourceFiles.push_back("/home/szymon/ClionProjects/speech-recognition/dataset/records/record_3_a.wav");
-//    sourceFiles.push_back("/home/szymon/ClionProjects/speech-recognition/dataset/records/record_4.wav");
-//    sourceFiles.push_back("/home/szymon/ClionProjects/speech-recognition/dataset/records/record_5.wav");
-//    sourceFiles.push_back("/home/kacper/voice/samogloski.wav");
-
-    std::string record0Content = getFileContent(
-            "/home/szymon/ClionProjects/speech-recognition/dataset/variants/words_0.txt");
-    str_replace(record0Content, ",", "");
-
-    std::string record1Content = getFileContent(
-            "/home/szymon/ClionProjects/speech-recognition/dataset/variants/words_1.txt");
-    str_replace(record1Content, ",", "");
-
-    std::string record2Content = getFileContent(
-            "/home/szymon/ClionProjects/speech-recognition/dataset/variants/words_2.txt");
-    str_replace(record2Content, ",", "");
-
-    std::string record3Content = getFileContent(
-            "/home/szymon/ClionProjects/speech-recognition/dataset/variants/words_3.txt");
-    str_replace(record3Content, ",", "");
-
-    std::string record4Content = getFileContent(
-            "/home/szymon/ClionProjects/speech-recognition/dataset/variants/words_4.txt");
-    str_replace(record4Content, ",", "");
-
-    std::string record5Content = getFileContent(
-            "/home/szymon/ClionProjects/speech-recognition/dataset/variants/words_5.txt");
-    str_replace(record5Content, ",", "");
-
-    std::vector<std::string> transcriptions;
-    transcriptions.push_back(record0Content);
-    transcriptions.push_back(record1Content);
-    transcriptions.push_back(record2Content);
-    transcriptions.push_back(record3Content);
-    transcriptions.push_back(record3Content);
-//    transcriptions.push_back(record4Content);
-//    transcriptions.push_back(record5Content);
-
-    std::vector<DataSource<short>> dataSources;
-    for (auto it = sourceFiles.begin(); it != sourceFiles.end(); it++) {
-        dataSources.push_back(WaveFileDataSource<short>(*it, singleSampleLength));
+    // get the same set once again and test what the model guesses about each utterance
+    {
+        WaveFileDataSource<short int> dataSource(filename, SAMPLE_LENGTH);
+        HMMLexicon::Observation utterance = mfccVectorizer->vectorize(dataSource);
+        lexicon.predict(utterance);
     }
 
-    std::vector<char> letters = getUniqueLetters(transcriptions);
-
-    shared_ptr<IVectorizer<short>> vectorizerPtr = shared_ptr<IVectorizer<short>>(
-//            new FormantVectorizer<short>());
-//            new ThirdsPowerVectorizer<short>());
-            new MFCCVectorizer<short>(26, 12));
-    shared_ptr<IClusteringMethod> clusteringMethodPtr = shared_ptr<IClusteringMethod>(
-//            new KMeans(numberOfPhonems, vectorizerPtr->getVectorSize()));
-            new GaussianMixtureModel(numberOfPhonems, vectorizerPtr->getVectorSize()));
-    shared_ptr<ISpellingTranscription> spellingMethodPtr = shared_ptr<ISpellingTranscription>(
-            new HMM(numberOfPhonems, letters));
-    LanguageModel<short> languageModel(vectorizerPtr, clusteringMethodPtr, spellingMethodPtr);
-
-//    std::ifstream input("/home/kacper/voice/model_Wed May 27 22:30:27 2015.lm");
-//    LanguageModel<short> languageModel(input);
-
-    try {
-        // fit the model using data taken from source files
-        languageModel.fit(dataSources, transcriptions);
-
-        // add a new file to be checked
-        dataSources.push_back(
-                WaveFileDataSource<short>("/home/kacper/voice/samogloski_100-110_100-110_100-110_100-110.wav",
-                                          singleSampleLength));
-        dataSources.push_back(
-                WaveFileDataSource<short>("/home/kacper/voice/samogloski_1_2_3_4_5_6_7_8_9_10_11_12-25.wav",
-                                          singleSampleLength));
-        dataSources.push_back(
-                WaveFileDataSource<short>("/home/kacper/voice/dziadzio.wav",
-                                          singleSampleLength));
-        dataSources.push_back(
-                WaveFileDataSource<short>("/home/kacper/voice/sala.wav",
-                                          singleSampleLength));
-
-        // test files one by one and try to predict the transcription
-        for (auto it = dataSources.begin(); it != dataSources.end(); it++) {
-            std::cout << "predicted: " << languageModel.predict(*it) << std::endl;
-        }
-
-//        char *modelFilename = new char[100];
-//        sprintf(modelFilename, "/home/kacper/voice/model_%s.lm", __TIMESTAMP__);
-//        std::ofstream output(modelFilename);
-//        output << languageModel;
-//        output.close();
-//        delete[] modelFilename;
-
-        // the model is fitted and can be used to transcript another data sources (validation)
-        // @todo: add a validation
-    } catch (speech::clustering::exception::TooLessVectorsException &ex) {
-        std::cerr << "You need to provide at least " << numberOfPhonems
-                  << " vectors to perform the clustering" << std::endl;
-    }*/
+    delete mfccVectorizer;
 
     return 0;
 }

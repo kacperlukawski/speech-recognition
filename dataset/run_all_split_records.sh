db1c12dd30f2cf4fa683cdd1a6477c9c870efda3
Szymon <szymonwenta@gmail.com>
2015-08-07 00:39:41 +0200

    Added run all split script.

---
+++ dataset/run_all_split_records.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# rm -r ./splitted_records/record0
+# mkdir -p ./splitted_records/record0
+# ./split_records.sh ./records/record_0.wav ./variants/words_0.txt ./splitted_records/record0 0.02 0.3
+
+
+# 11
+rm -r ./splitted_records/record11
+mkdir -p ./splitted_records/record11
+./split_records.sh ./records/record_11.wav ./variants/words_11.txt ./splitted_records/record11 0.01 0.3
--- dataset/split_records.sh
+++ dataset/split_records.sh
@@ -3,29 +3,46 @@
 file_to_split=$1
 description_file=$2
 destination_dir=$3
-silence_duration=$4
+duration=$4
+silence_threshold=$5
 
 if [ -z "$file_to_split" ] || [ -z "$description_file" ] || [ -z "$destination_dir" ]
 then
   echo "ERROR: Incorrect arguments"
-  echo "Syntax: ./split_records.sh file_to_split description_file destination_dir [silence_duration=0.5]"
+  echo "Syntax: ./split_records.sh file_to_split description_file destination_dir [duration=0.5 silence_threshold=0.3]"
   echo "Example: ./split_records.sh ./records/record_22.wav ./variants/words_22.txt ./test 0.01"
   echo "To run this script you have to install sox (sudo apt-get install sox)"
 else
-  if [ -z "$silence_duration" ]
+  if [ -z "$duration" ]
   then
-    silence_duration=0.5
+    duration=0.5
   else
     reg_exp='^[0-9]+([.][0-9]+)?$'
-    if ! [[ $silence_duration =~ $reg_exp ]] 
+    if ! [[ $duration =~ $reg_exp ]] 
     then
-      echo "ERROR: Variable silence_duration must be a number.";
+      echo "ERROR: Variable duration must be a number.";
+      exit 1;
+    fi
+  fi
+  if [ -z "$silence_threshold" ]
+  then
+    silence_threshold=0.5
+  else
+    reg_exp='^[0-9]+([.][0-9]+)?$'
+    if ! [[ $silence_threshold =~ $reg_exp ]] 
+    then
+      echo "ERROR: Variable silence_threshold must be a number.";
       exit 1;
     fi
   fi
 
   tmp="$destination_dir/tmp_.wav"
-  sox $file_to_split $tmp silence 1 0.2 1% 1 $silence_duration 1% : newfile : restart
+  
+  # TODO Tu parametry sa zle przekazywane! Ale dziwnym trafem dla nagrania 11 byly optymalne, wiec zostawiam
+  sox $file_to_split $tmp silence 1 $silence_threshold 1% 1 $duration 1% : newfile : restart
+  # TODO Tu juz parametry lepiej wygladaja, ale w ogole ich dobrac dobrze nie moge... 
+  # trzeba to zgrac z dokumentacja http://sox.sourceforge.net/sox.html#OPTIONS silence
+  #sox $file_to_split $tmp silence 1 $duration $silence_threshold% 1 $duration $silence_threshold% : newfile : restart
  
   words=$(head -n 1 $description_file)
   IFS=',' read -a wordsArr <<< "$words"

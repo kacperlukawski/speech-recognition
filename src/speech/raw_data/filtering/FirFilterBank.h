d2e0754fb6e1841c0ae453f24760829e548b864c
szymon <szymonwenta@gmail.com>
2015-09-11 22:18:52 +0200

    Added new FIR filter types.

--- src/speech/raw_data/filtering/FirFilterBank.h
+++ src/speech/raw_data/filtering/FirFilterBank.h
@@ -71,7 +71,87 @@ namespace speech {
                  */
                 static const FirFilterData LOWPASS_44100_1000_3DB_80DB;
 
-                // TODO add more filters
+                /**
+                 * {
+                        gain: 0, // stopband
+                        atten: -80dB,
+                        start: 0Hz,
+                        stop: 2000Hz
+                    },
+                            
+                    {
+                        gain: 1, // passband
+                        ripple: 3dB,
+                        start: 3000Hz,
+                        stop: 22000Hz
+                    }
+                 */
+                static const FirFilterData HIGHPASS_44100_3000_3DB_80DB;
+
+                /**
+                 * {
+                        gain: 0, // stopband
+                        atten: -80dB,
+                        start: 0Hz,
+                        stop: 7000Hz
+                    },
+                            
+                    {
+                        gain: 1, // passband
+                        ripple: 3dB,
+                        start: 8000Hz,
+                        stop: 22000Hz
+                    }
+                 */
+                static const FirFilterData HIGHPASS_44100_8000_3DB_80DB;
+
+                /**
+                 * {
+                        gain: 0, // stopband
+                        atten: -60dB,
+                        start: 0Hz,
+                        stop: 1000Hz
+                    },
+                            
+                    {
+                        gain: 1, // passband
+                        ripple: 5dB,
+                        start: 2000Hz,
+                        stop: 5000Hz
+                    },
+
+                    {
+                        gain: 0, // stopband
+                        atten: -60dB,
+                        start: 6000Hz,
+                        stop: 22000Hz
+                    }
+                 */
+                static const FirFilterData BANDPASS_44100_2000_5000_5DB_60DB;
+
+                /**
+                 * {
+                        gain: 1, // passband
+                        ripple: 3dB,
+                        start: 0Hz,
+                        stop: 1000Hz
+                    },
+                            
+                    {
+                        gain: 0, // stopband
+                        atten: -60dB,
+                        start: 2000Hz,
+                        stop: 5000Hz
+                    },
+
+                    {
+                        gain: 1, // passband
+                        ripple: 3dB,
+                        start: 6000Hz,
+                        stop: 22000Hz
+                    }
+                 */
+                static const FirFilterData BANDSTOP_44100_2000_5000_3DB_60DB;
             };
         }
     }

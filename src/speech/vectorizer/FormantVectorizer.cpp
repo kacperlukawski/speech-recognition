0b15a4c6b7269bb7c696ea76092c2c522d91023e
kacper <kacper.lukawski@uj.edu.pl>
2015-09-13 18:34:38 +0200

    Updated comments and frequencies

--- src/speech/vectorizer/FormantVectorizer.cpp
+++ src/speech/vectorizer/FormantVectorizer.cpp
@@ -21,6 +21,9 @@ speech::vectorizer::FormantVectorizer<FrameType>::FormantVectorizer() {
 
 template<typename FrameType>
 std::valarray<double> speech::vectorizer::FormantVectorizer<FrameType>::vectorize(FrequencySample<FrameType> &sample) {
+    // F0 boundaries
+    int pos100Hz = sample.getFrequencyIndex(100.0);
+
     // F1 boundaries
     int pos300Hz = sample.getFrequencyIndex(300.0);
     int pos1000Hz = sample.getFrequencyIndex(1000.0);
@@ -33,29 +36,17 @@ std::valarray<double> speech::vectorizer::FormantVectorizer<FrameType>::vectoriz
     int pos2350Hz = sample.getFrequencyIndex(2350.0);
     int pos4000Hz = sample.getFrequencyIndex(4000.0);
 
-    // F3 boundaries
-    int pos5000Hz = sample.getFrequencyIndex(5000.0);
-    int pos15000Hz = sample.getFrequencyIndex(15000.0);
-
     double *amplitudePtr = sample.getAmplitude().get();
+    double *f0FormantPos = std::max_element(amplitudePtr + pos100Hz, amplitudePtr + pos300Hz);
     double *f1FormantPos = std::max_element(amplitudePtr + pos300Hz, amplitudePtr + pos1000Hz);
     double *f2FormantPos = std::max_element(amplitudePtr + pos850Hz, amplitudePtr + pos2500Hz);
     double *f3FormantPos = std::max_element(amplitudePtr + pos2350Hz, amplitudePtr + pos4000Hz);
-    double *f4FormantPos = std::max_element(amplitudePtr + pos5000Hz, amplitudePtr + pos15000Hz);
 
     std::valarray<double> result(0.0, 4);
-//    result[0] = *f1FormantPos;
-//    result[1] = *f2FormantPos;
-    if (*f1FormantPos > 0.25)
-        result[0] = (sample.getIndexFrequency(f1FormantPos - amplitudePtr));
-    if (*f2FormantPos > 0.25)
-        result[1] = (sample.getIndexFrequency(f2FormantPos - amplitudePtr));
-    if (*f3FormantPos > 0.25)
-        result[2] = (sample.getIndexFrequency(f3FormantPos - amplitudePtr));
-    if (*f4FormantPos > 0.25)
-        result[3] = (sample.getIndexFrequency(f4FormantPos - amplitudePtr));
-
-//    std::cout << result[2] << " " << result[3] << std::endl;
+    result[0] = (sample.getIndexFrequency(f0FormantPos - amplitudePtr));
+    result[1] = (sample.getIndexFrequency(f1FormantPos - amplitudePtr));
+    result[2] = (sample.getIndexFrequency(f2FormantPos - amplitudePtr));
+    result[3] = (sample.getIndexFrequency(f3FormantPos - amplitudePtr));
 
     return result;
 }
@@ -68,7 +59,7 @@ void speech::vectorizer::FormantVectorizer<FrameType>::serialize(std::ostream &o
 
 template<typename FrameType>
 int speech::vectorizer::FormantVectorizer<FrameType>::getVectorSize() const {
-    return 4; // TODO: get real vector size
+    return 4;
 }
 
 template
